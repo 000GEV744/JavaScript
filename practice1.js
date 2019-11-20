8173fd760fb7dbff23b4b58afdf250ec85661f3a
Anuj Kumar Singh <anuj60361@gmail.com>
2019-11-20 17:22:46 +0530

    Theory part of javascript

---
+++ practice1.js
@@ -0,0 +1,34 @@
+var num = 1; //number 
+var name = "Anuj Singh";//string
+var flag = true;//boolean
+
+name="Ambikesh"
+var result = 1 + 4 + "anuj Singh" +4 + 6;  //output : 5anuj Singh46
+//when the program execution comes to the string then it will take the remaining as string.
+document.write(result);
+document.write(name);
+alert(num);
+//theme that we are using in vscode is "monokai contrast"
+//JS is loosely typed as we dont have to define the datatype 
+//JS is dynamically typed as whatever the data you will pass depending on that the datatype of variable will change
+
+
+/* in javaScript identifiers known as the name that you give to the 
+variables and the rules are as follows :
+1. the names should not be any predefined keyword.
+2. the first character must be letter, dollar or an underscore.
+3. varibales are case-sensitive.*/
+
+/*Associativity :
+  = is showing right to left associativity.
+*/
+
+// conditional statements :
+// if-else
+// switch case
+
+// looping statements:
+// for loop 
+// while loop 
+// for..in loop 
+// do while loop
