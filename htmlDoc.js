b7b417ee5aadded9f312d321fb6b0c3b3665ab43
Anuj Kumar Singh <anuj60361@gmail.com>
2019-11-20 16:19:51 +0530

    hello world program in js

---
+++ htmlDoc.js
@@ -0,0 +1,3 @@
+document.write("Hello world"); 
+//document is basically the object that represents the entire document and we can also write the html tag inside the write() fuction
+document.write("<h2>Hi, this is anuj external</h2>")
