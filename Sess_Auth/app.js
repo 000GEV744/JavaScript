8ffc610a5bca0ddac7c924cc6c20a92b3a992c1c
Anuj Kumar Singh <anuj60361@gmail.com>
2020-01-24 18:58:17 +0530

    Session Management

--- Sess_Auth/app.js
+++ Sess_Auth/app.js
@@ -10,7 +10,7 @@ const {
   SESS_NAME = "sid",
   SESS_SECRET = "helloThisIsKey"
 } = process.env;
-const users = [
+let users = [
   {
     id: 1,
     name: "Anuj Singh",
@@ -58,6 +58,12 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  const userId = req.session.userId;
+  const user = users.find(user => user.id === userId);
+  res.locals.user = user;
+  next();
+});
 ///middleware function for redirection to login incase user try to go diretcly to the home page
 const redirectLogin = (req, res, next) => {
   if (!req.session.userId) {
@@ -75,6 +81,7 @@ const redirectHome = (req, res, next) => {
   }
 };
 
+//********************MAIN PAGE********** */
 app.get("/", (req, res) => {
   const { userId } = req.session; //checking for whether the session contains the userid or not!
   res.send(
@@ -91,18 +98,25 @@ app.get("/", (req, res) => {
   );
 });
 
+//******************HOME PAGE ****************** */
 app.get("/home", redirectLogin, (req, res) => {
+  const user = res.locals.user;
+  console.log(user);
   res.send(
     `<h1>  Home </h1>
-      <a href ="/"> Main<a>
+      <a href ="/"> Main<a> &nbsp
+      <form method = "POST" action = "/logout">
+      <button>Logout</button>
+     </form>
       <ul>
-      <li> name : </li>
-      <li> email: </li>
+      <li> name : ${user.name}</li>
+      <li> email: ${user.email}</li>
       </ul>
       `
   );
 });
 
+//************LOGIN PAGE ******************* */
 app.get("/login", redirectHome, (req, res) => {
   res.send(`
   <h2>Login</h2>
@@ -118,6 +132,7 @@ app.get("/login", redirectHome, (req, res) => {
     `);
 });
 
+//***************REGISTER PAGE****************/
 app.get("/register", (req, res) => {
   res.send(
     `<h2>Register</h2>
@@ -138,39 +153,45 @@ app.get("/register", (req, res) => {
 
 /***************LOGIN USER*****************/
 app.post("/login", redirectHome, (req, res) => {
+  console.log(req.session.userId + " " + req.body);
   const { email, password } = req.body;
+  console.log(email + " " + password);
   if (email && password) {
-    const user = users.find(user => {
-      user.email == email && user.password == password;
+    const user = users.find(u => {
+      if (u.email == email && u.password == password) {
+        return u;
+      }
     });
+    console.log(user);
     if (user) {
       req.session.userId = user.id;
       res.redirect("/home");
+    } else {
+      res.redirect("/login");
     }
+  } else {
+    res.redirect("/login");
   }
 });
 
 /************REGISTER USER*****************/
-app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  if (name && email && password) {
-    const exists = users.some(() => {
-      users.email == email;
-    });
+app.post("/register", redirectHome, (req, res) => {
+  const { name, email, pwd } = req.body;
+  if (name && email && pwd) {
+    const exists = users.some(user => user.email === email);
     if (!exists) {
-      const toAddUser = {
+      const user = {
         id: users.length + 1,
         name: name,
         email: email,
-        password: password
+        password: pwd
       };
-      users.push(toAddUser);
-      req.session.userId = toAddUser.id;
-      res.redirect("/home");
+      users.push(user);
+      req.session.userId = user.id;
+      return res.redirect("/home");
     }
   }
-
-  res.redirect("/redirect");
+  res.redirect("/register");
 });
 
 /*************LOGOUT THE USER***********************/
@@ -179,8 +200,9 @@ app.post("/logout", redirectLogin, (req, res) => {
     if (err) {
       res.redirect("/home");
     }
-    
   });
+  res.clearCookie(SESS_NAME);
+  res.redirect("/");
 });
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
